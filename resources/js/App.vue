1f05e9f9b795687a222f03f39a608073175f85b5
Ceebot <mihailceeb8@mail.ru>
2023-11-23 18:02:05 +0700

    create project

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>
+        {{ number }}
+    </h1>
+    <button @click="clickNumber">Увеличить</button>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+const number = ref(0);
+
+function clickNumber() {
+    number.value += 1;
+}
+
+</script>
